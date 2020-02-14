c5dd97ac23d95becb2b11ab0f1f7783dadc1c409
dattebayoRob <robson.william65@gmail.com>
2020-02-13 21:07:06 -0300

    Initial commit

---
+++ src/main/java/com/dtb/jpadeletetest/SpringDataJpaDeleteTestsApplication.java
@@ -0,0 +1,13 @@
+package com.dtb.jpadeletetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaDeleteTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaDeleteTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dtb/jpadeletetest/SpringDataJpaDeleteTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtb.jpadeletetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaDeleteTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
